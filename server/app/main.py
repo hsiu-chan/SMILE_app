1c3f8beead9c918af70d4896eef4e08c7524aeff
chc <106435999+hsiu-chan@users.noreply.github.com>
2024-01-10 22:24:04 +0800

    modified 3 files

--- server/app/main.py
+++ server/app/main.py
@@ -7,6 +7,6 @@ app = create_app()
 if __name__ == '__main__':
     #app.run(host='0.0.0.0',port=8888)
     from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 8888, app)
+    run_simple('192.168.50.241', 8888, app)
 
     #app.run(host=,port=8888)
